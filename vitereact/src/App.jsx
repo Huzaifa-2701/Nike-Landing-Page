b37a33e56e868b073f52665a01e0082b4e870c61
Huziafa-2701 <Huzaifa-2701>
2025-01-13 17:07:07 +0500

    first commit

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navigation from './components/Navigation';
+import "./App.css";
+import HeroSection from './components/HeroSection';
+
+const App = () => {
+  return (
+    <div>
+     <Navigation />
+     <HeroSection />
+    </div>
+  )
+}
+
+export default App
